2c66bfdea3b882678a49421c1d9cbf0e63105a8c
Michelle <falbqui495@g.educaand.es>
2023-11-07 09:18:33 +0100

    Add tests 1/2 to Excersice2Test.java

--- src/test/java/org/iesvdm/kata1/Exercise1Test.java
+++ src/test/java/org/iesvdm/kata1/Exercise1Test.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class Exercise1Test extends PetDomainForKata
 {
